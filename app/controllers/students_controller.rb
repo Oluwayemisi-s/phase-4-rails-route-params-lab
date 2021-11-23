dd19f36456e5c806ceea1e75afb3e6e156ceba7b
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-11-22 17:12:47 -0800

    commit tree d49f56c69a11c6a865a4026e851d166ab9f3c325

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name]
+                students = Student.where("first_name = ? or last_name = ?", params[:name].capitalize, params[:name].capitalize)
+              else
+                Student.all
+              end
     render json: students
+    
+  end
+
+  def student
+    student = Student.find_by(id: params[:id])
+    render json: student 
   end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,8 @@ class Student < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  # def self.search_name(name)
+  #   students = Student.where("first_name = params[:name]") || Student.where("last_name = params[:name]")
+  # end
+
 end
